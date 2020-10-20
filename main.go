6cce132f2569fe4b2d70f93d9801bd081307e560
Udaya N <udayaprakash2899@gmail.com>
2020-10-20 19:31:01 +0530

    Added comments

--- main.go
+++ main.go
@@ -7,13 +7,17 @@ import (
 )
 
 func main() {
+	// initiates a gin Engine with the default logger and recovery middleware
 	router := gin.Default()
 
+	// sets up a GET API in route /hello that returns the text "World"
 	router.GET("/hello", func(c *gin.Context) {
 		c.JSON(http.StatusOK, gin.H{
 			"message": "World",
 		})
 	})
 
+	// Run implements a http.ListenAndServe() and takes in an optional Port number
+	// The default port is :8080
 	router.Run()
 }
